23fe73757b285c3af9756d04d06a569ef9ca94a8
jhoselinteranc <jhoselinteranc@gmail.com>
2024-07-19 23:14:31 +0000

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import MyButton from './MyButtom.tsx';
+
+function App() {
+  return (
+    <>
+      <h1>React App</h1>
+      <MyButton />
+    </>
+  )
+}
+
+export default App
+
